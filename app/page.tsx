decbc475e9a32dc47520dcc2de23aa0c22bbd253
AbdelrahmanZood <20007@zood.ai>
2025-04-24 19:24:06 +0200

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Page = () => {
+  return (
+    <main>
+      <header className="flex items-center">
+        <p>OpenAI</p>
+        <button title="Subscribe to updates" type="button">
+          Subscribe to updates
+        </button>
+      </header>
+    </main>
+  );
+};
+export default Page;
